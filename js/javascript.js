9c25743ee155e1cb9aa31df10a87631fd55baee3
EduardoHRVitorino <prometheus29_09@proton.me>
2024-04-02 08:15:38 -0300

    02/04/2024

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+var mensagem =  String("hello nice to meet you!")
+var resposta = window.prompt("are you a boy or a girl?")
+switch (resposta) {
+    case "boy":
+
+        window.alert("hello boy")
+        
+        
+        break;
+
+
+    case "girl":
+        window.alert("hello girl")
+        break;
+    default:
+        break;
+}
+alert(mensagem)
+
